4c24d4e0c35c9416e3919199d6e9ee468a8fd104
Davide Bizzarri <dbizzarr@redhat.com>
2021-04-05 15:52:03 +0200

    Create landing page draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Alert } from "shards-react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Container } from "shards-react";
+import logo from "./logo-white.svg";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,7 +7,37 @@ import "shards-ui/dist/css/shards.min.css";
 
 function App() {
   return (
-    <Alert></Alert>
+    <div className="landing d-flex justify-content-center flex-column">
+      <Container>
+        <Navbar type="dark" expand="md">
+          <NavbarBrand href="#">
+            <img src={logo} className="logo mr-2" />
+            Polaris
+          </NavbarBrand>
+          <Nav navbar>
+            <NavItem>
+              <NavLink active href="#">
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Store</NavLink>
+            </NavItem>
+          </Nav>
+          <Nav navbar className="ml-auto">
+            <NavLink>Cart</NavLink>
+          </Nav>
+        </Navbar>
+      </Container>
+      <div className="inner-wrapper mt-auto mb-auto container">
+          <div className="row">
+            <div className="col-md-7">
+                <h1 className="welcome-heading display-4 text-white">Gelateria Polaris</h1>
+                <p className="text-white">dal 1989</p>
+            </div>
+          </div>
+        </div>
+    </div>
   );
 }
 
