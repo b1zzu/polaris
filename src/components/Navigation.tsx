3f3b60ed7e01241638a13d2d6dde1d065291800b
Davide Bizzarri <dbizzarr@redhat.com>
2021-04-06 19:27:28 +0200

    Make navbar collapsable

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,24 @@
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "shards-react";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Collapse,
+  NavbarToggler,
+} from "shards-react";
 import logo from "../logo-white.svg";
 import { useTranslation } from "react-i18next";
 import "./Navigation.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navigation() {
   const { t } = useTranslation();
+
+  const [collapse, setCollapse] = useState<Boolean>();
+
+  const toggleNavbar = () => setCollapse(!collapse)
 
   return (
     <Navbar type="dark" expand="md">
@@ -13,19 +26,27 @@ function Navigation() {
         <img src={logo} className="logo mr-2" alt="" />
         Polaris
       </NavbarBrand>
-      <Nav navbar>
-        <NavItem>
+      <NavbarToggler onClick={toggleNavbar} />
+
+      <Collapse open={collapse} navbar>
+        <Nav navbar>
+          <NavItem>
             <NavLink active tag={Link} to="/">
               Home
             </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink tag={Link} to="/products">{t("Products")}</NavLink>
-        </NavItem>
-      </Nav>
-      <Nav navbar className="ml-auto">
-        <NavLink tag={Link} to="/cart">{t('Cart')}</NavLink>
-      </Nav>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/products">
+              {t("Products")}
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <Nav navbar className="ml-auto">
+          <NavLink tag={Link} to="/cart">
+            {t("Cart")}
+          </NavLink>
+        </Nav>
+      </Collapse>
     </Navbar>
   );
 }
