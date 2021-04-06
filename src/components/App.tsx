0762fcff2cd96b72a82553236e1008d91493036d
Davide Bizzarri <dbizzarr@redhat.com>
2021-04-06 19:13:30 +0200

    Add react-router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,28 +3,53 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
 import { useTranslation } from "react-i18next";
-import Navigation from "./Navigation"
+import Navigation from "./Navigation";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Landing() {
   const { t } = useTranslation();
 
   return (
     <div className="landing d-flex justify-content-center flex-column">
       <Container>
-        <Navigation/>
+        <Navigation />
       </Container>
       <Container className="mt-auto mb-auto">
         <Row>
           <Col>
-            <h1 className="display-4 text-white">
-              {t("Gelateria Polaris")}
-            </h1>
+            <h1 className="display-4 text-white">{t("Gelateria Polaris")}</h1>
             <p className="text-white">{t("dal 1989")}</p>
           </Col>
         </Row>
       </Container>
     </div>
   );
+}
+
+function Products() {
+  return <h1>Products</h1>;
+}
+
+function Cart() {
+  return <h1>Cart</h1>;
 }
 
 export default App;
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,28 +2,29 @@ import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "shards-react";
 import logo from "../logo-white.svg";
 import { useTranslation } from "react-i18next";
 import "./Navigation.css";
+import { Link } from "react-router-dom";
 
 function Navigation() {
   const { t } = useTranslation();
 
   return (
     <Navbar type="dark" expand="md">
-      <NavbarBrand href="#">
+      <NavbarBrand tag={Link} to="/">
         <img src={logo} className="logo mr-2" alt="" />
         Polaris
       </NavbarBrand>
       <Nav navbar>
         <NavItem>
-          <NavLink active href="#">
-            Home
-          </NavLink>
+            <NavLink active tag={Link} to="/">
+              Home
+            </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="#">{t("Store")}</NavLink>
+            <NavLink tag={Link} to="/products">{t("Products")}</NavLink>
         </NavItem>
       </Nav>
       <Nav navbar className="ml-auto">
-        <NavLink>Cart</NavLink>
+        <NavLink tag={Link} to="/cart">{t('Cart')}</NavLink>
       </Nav>
     </Navbar>
   );
